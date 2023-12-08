738244197fe97731c106d10acea7f1a4f14a8909
Nico-Mayer <nico.m94@icloud.com>
2023-12-08 11:49:28 +0100

    day09 init

---
+++ days/day_09/part_01.go
@@ -0,0 +1,14 @@
+package day_09
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nico-mayer/aoc_2023/utils"
+)
+
+func Part01() {
+	data := strings.Split(utils.GetData("09", true), "\n")
+
+	fmt.Printf("%#v", data)
+}
